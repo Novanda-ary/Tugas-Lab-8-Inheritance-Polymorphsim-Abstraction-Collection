e9b23eed4dafb465b775347b04e8a9cf820fd591
Novanda-ary <61939867+Novanda-ary@users.noreply.github.com>
2020-06-23 21:34:26 +0700

    Update Sales.cs

--- Sales.cs
+++ Sales.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Inheritance__Polymorphsim__Abstraction___Collection_2685
+namespace Inheritance__Polymorphsim__Abstraction___Collection_2695
 {
     class Sales : Karyawan
     {
